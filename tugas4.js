6b8cd0b62448f97831ffa7f5989e02d3648817a7
mazajiputra <mazajiputra@gmail.com>
2021-10-29 20:55:57 +0700

    Kumpul Tugas

---
+++ tugas4.js
@@ -0,0 +1,19 @@
+siswaA=171;
+siswaB=170;
+siswaC=172;
+
+if (siswaA>siswaB){
+    if (siswaA>siswaC) {
+        console.log("Siswa A yang tertinggi");
+    }
+    else{
+        console.log("Siswa A tingginya diantara kedua siswa lainnya");
+    }
+}else{
+    if (siswaA<siswaC) {
+        console.log("Siswa A yang terpendek");
+    }
+    else{
+        console.log("Siswa A tingginya diantara kedua siswa lainnya");
+    }
+}
